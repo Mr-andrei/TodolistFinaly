bf33f621635d1d7750926fbdde84392a2c5c31d6
Mr-andrei <91520213+Mr-andrei@users.noreply.github.com>
2022-04-11 20:20:33 +0300

    Updates

---
+++ precache-manifest.c7a45f1abc76625ca2ced52dd0ece143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41d9dcaf4af70f56b56240ab59b1eb0",
+    "url": "/TodolistFinaly/index.html"
+  },
+  {
+    "revision": "5e9020cec447c325ce47",
+    "url": "/TodolistFinaly/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/TodolistFinaly/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodolistFinaly/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9020cec447c325ce47",
+    "url": "/TodolistFinaly/static/js/main.3628da1b.chunk.js"
+  },
+  {
+    "revision": "a6c5632b9fc40921faed",
+    "url": "/TodolistFinaly/static/js/runtime-main.48d13a8c.js"
+  }
+]);
