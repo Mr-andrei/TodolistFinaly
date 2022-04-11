22330102d4f3b065b81ebd55c246bc7802dd5e86
Mr-andrei <91520213+Mr-andrei@users.noreply.github.com>
2022-04-11 20:17:27 +0300

    new commit

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': 'd832e30a-b023-4ff8-b5a8-9ef96cfd6279'
     }
 }
 const instance = axios.create({
